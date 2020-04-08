6bd7eb950862e82c91b6a8029942df67a16bc020
Eric Harding <eric.harding@bluetradesys.com>
2020-04-07 20:49:13 -0400

    Bloom filters

---
+++ April2020/Program.fs
@@ -0,0 +1,35 @@
+﻿open System.Collections
+
+let words = System.IO.File.ReadAllLines("words.txt")
+
+let capacity = 1024*1024
+
+let hashBit word =
+  abs(hash word % capacity)
+
+let addToBloom (bloom:BitArray) word =
+  let bit = hashBit word
+  bloom.[bit] <- true
+  bloom
+
+let bloom = 
+  words
+  |> Array.fold addToBloom (BitArray(capacity))
+
+let isSpelledCorrectly word =
+  let bit = hashBit word
+  bloom.[bit]
+
+
+isSpelledCorrectly "hello"
+isSpelledCorrectly "filter"
+
+// #time
+// [|for i in bloom -> i|] 
+// |> Seq.groupBy id 
+// |> Seq.map (fun (v,a) -> v, Seq.length a)
+
+// let _ =
+//   [for i in bloom -> i] 
+//   |> Seq.groupBy id 
+//   |> Seq.map (fun (key,values) -> key, Seq.length values)
